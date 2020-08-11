1a2c25997418c4e527f30571caee3db1fba61943
Lieven Van Loo <lieven.vanloo@student.hogent.be>
2020-08-11 17:16:19 +0200

    api & authorisatie

--- Front/forumApp/src/app/app.module.ts
+++ Front/forumApp/src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MaterialModule} from './material/material.module';
+import { UserModule } from './user/user.module';
+import { httpInterceptorProviders } from './interceptors';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    UserModule,
   ],
-  providers: [],
+  providers: [httpInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
